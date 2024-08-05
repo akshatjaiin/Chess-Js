04ed4a06662c40a73aa0fad877d572095f6347b5
Akshat Jain <akshat0@yahoo.com>
2024-08-05 18:41:45 +0530

    chess 8

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const gameBoard = document.querySelector('#gameBoard')
+const gameBoard = document.querySelector('#gameboard')
 const plaerDisplay = document.querySelector('#player')
-const infoDisplay = document.querySelector('#infoDisplay')
+const infoDisplay = document.querySelector('#info-display')
 const width = 8
 
 const startPieces = [
@@ -20,10 +20,47 @@ function createBoard() {
     const square = document.createElement('div')
     square.classList.add('square')
     square.innerHTML = startPiece
+    square.firstChild?.setAttribute('draggable', true)
     square.setAttribute('square-id', i)
-    square.classList.add('beige')
+   // square.classList.add('beige')
+   const row = Math.floor((63 - i) / 8 + 1)
+   if (row % 2 === 0) {
+    square.classList.add(i % 2 === 0 ? "brown" : "beige");
+  } else {
+    square.classList.add(i % 2 === 0 ? "beige" : "brown");
+  }
+   
+  if (i <= 15 && startPiece !== '') {
+    square.firstChild.classList.add('black')
+  }
+  if (i >= 48 && startPiece !== '') {
+    square.firstChild.classList.add('white')
+  }
     gameBoard.append(square)
   })
 }
 
 createBoard()
+
+const allSquares = document.querySelectorAll('#gameboard .square')
+
+allSquares.forEach(square => {
+  square.addEventListener('dragstart', dragStart)
+  square.addEventListener('dragover', dragOver)
+  square.addEventListener('drop', dragDrop)
+
+})
+let startPositionId
+function dragStart (e) {
+  startPositionId = console.log(e.target.parentNode.getAttribute('square-id'))
+}
+
+function dragOver(e) {
+  e.preventDefault()
+}
+
+function dragDrop(e) {
+  e.stopPropagation()
+
+  e.target.parentNode.append
+}
